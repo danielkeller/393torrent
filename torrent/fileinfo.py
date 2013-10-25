8e786ca54e9d96fb153ca8273ff3dc8dffeacea0
Nathan McKinley <nath@nmckinley.com>
2013-10-25 16:12:46 -0400

    initial work on tracker and file reading

--- torrent/fileinfo.py
+++ torrent/fileinfo.py
@@ -1,8 +1,57 @@
+import bencode
+import hashlib
+import requests
+
 class TorrentFileInfo(object):
-    pass
+    def __init__(self, torrent_file_text):
+        torrent_dict = bencode.bdecode(torrent_file_text)
+        self.tracker = TorrentTracker(torrent_dict['announce'])
+        self.comment = torrent_dict.get('comment')
+        self.created = time.localtime(torrent_dict.get('creation date'))
+        self.author = torrent_dict.get('created by')
+        pieces = torrent_dict['info']['pieces']
+        self.pieces = [pieces[i:i+20] for i in range(0, len(pieces), 20)]
+        self.info_hash = self.find_info_hash(torrent_file_text)
+        self.files = self.get_files_from_info_dict(torrent_dict['info'])
+
+
+    def find_info_hash(self, text):
+        info_dict_start = text.find('4:infod') + len('4:info')
+        success = False
+        end = info_dict_start
+        while not success and end <= len(text):
+            end += 1
+            try:
+                bencode.bdecode(text[info_dict_start:end])
+                break
+            except BTFailure:
+                pass
+        info_string = text[info_dict_start:end]
+        return hashlib.sha1(info_string).digest()
+
+    def get_files_from_info_dict(self, info_dict):
+        downloadfile = namedtuple('length', 'name')
+        if 'files' in info_dict:
+            return [
+                    downloadfile(f['length'],os.path.join([info_dict['name']] + f['path']))
+                    for f in info_dict['files']
+                   ]
+        else:
+            return [downloadfile(info_dict['length'], info_dict['name'])]
+
 
 class TorrentTracker(object):
-    pass
+    def __init__(self, tracker_url):
+        self.tracker_url = tracker_url
+
+    def initiate_download(self):
+        pass
+
+    def get_peer_list(self):
+        pass
+
+    def update_tracker_on_progress(self, amount_downloaded, amount_uploaded):
+        pass
 
 class TorrentPeer(object):
     pass
--- torrent/torrent.py
+++ torrent/torrent.py
@@ -1,10 +1,14 @@
 import os
 import argparse
+import bencode
 
 class TorrentApplication(object):
     def __init__(self, torrent_file, seed):
         self.file_info = TorrentFileRetriever(torrent_file).retrieve()
         self.seed = seed
+
+    def start(self):
+
 
 class TorrentFileRetriever(object):
     def __init__(self, torrent_file):
@@ -13,7 +17,19 @@ class TorrentFileRetriever(object):
     def retrieve(self):
         # either read the torrent file if it's a file or
         # download the file at the magnet link
-        pass
+        try:
+            file_text = open(self.torrent_file).read()
+            return self.handle_torrent_file(file_text)
+        except IOError:
+            return self.handle_magnet_link(self.torrent_file)
+
+    def handle_torrent_file(self, torrent_file_text):
+        return TorrentFileInfo(torrent_file_text)
+
+    def handle_magnet_link(self, magnet_link_url):
+        raise NotImplemented
+
+
 
 class TorrentDownloader(object):
     pass
@@ -29,4 +45,4 @@ if __name__ == '__main__':
                    default=False,
                    help='seed forever (default: seed while downloading)')
     parser.parse_args()
-    TorrentApplication(parser.torrent_file, parser.seed)
+    TorrentApplication(parser.torrent_file, parser.seed).start()
