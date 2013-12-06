6efb56a9132b1ab18199e17dcc6059520eb9940d
Ross Nanopoulos <rdn21@case.edu>
2013-12-06 15:29:04 -0500

    Added in template for testing FilePieces and FileBlocks

---
+++ test/files_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+class FilePieceTest(unittest.TestCase):
+    def test_init(self):
+        pass
+
+    def test_add_block(self):
+        pass
+    
+    def test_get_next_block_id(self):
+        pass
+
+    def test_is_fully_requested(self):
+        pass
+    
+    def test_is_fully_downloaded(self):
+        pass
+
+    def test_verify_and_write(self):
+        pass
+
+class FileBlockTest(unittest.TestCase):
+    def test_init(self):
+        pass
